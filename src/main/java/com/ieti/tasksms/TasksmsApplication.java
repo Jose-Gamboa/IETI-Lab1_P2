7254f7e8979d318a7105a8f6ea25dd0bbeb91305
Jose Gamboa <jose.gamboa@mail.escuelaing.edu.co>
2022-08-27 21:53:44 -0500

    First commit

---
+++ src/main/java/com/ieti/tasksms/TasksmsApplication.java
@@ -0,0 +1,13 @@
+package com.ieti.tasksms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ieti/tasksms/TasksmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ieti.tasksms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
